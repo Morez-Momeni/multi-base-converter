40fbbf8fb868a720b5eb5f6311dc8328910eae90
Morez <rmomeni693@gmail.com>
2025-10-25 01:56:03 +0330

    Update multi-base-converter.py
    Add new feature to multi-base converter ( binary to decimal )

--- multi-base-converter/multi-base-converter.py
+++ multi-base-converter/multi-base-converter.py
@@ -1,5 +1,5 @@
-def change_to_binary(num: int): 
-    #تبدیل مبنا از دسیمال به باینری 
+def change_to_binary(num: int) -> str: 
+    #Convert a decimal number to a binary number.
     
     binary_saver = []
     one_zero_saver = []
@@ -7,7 +7,7 @@ def change_to_binary(num: int):
     result=""
        
     if num == 0:
-        return 0
+        return "0b0"
     elif num < 0:
         
         num*=-1
@@ -61,21 +61,70 @@ def change_to_binary(num: int):
                 break
             
         return result        
+
+def change_to_decimal(num: int):
    
+    """
+    Convert a binary number to a decimal number.
+    
+    Note:
+        This function currently only supports positive numbers.
         
+        Negative numbers are not handled in this script. 
+        Implementation of the following numeric systems will be done in a separate repository:
+            - One's Complement
+            - Two's Complement
+            - Unsigned (no sign)
+        Addition and subtraction operations in these systems will also be implemented there.
+    """
+    
+  
+    save_binary = []
+    sum_bit_value = 0
+    
+    if num == 0 :
+        return 0
+    elif num < 0 :
+          raise NotImplementedError(
+            "Negative numbers are not supported in this function. "
+            "This feature will be implemented in the separate complement systems repository."
+        )
+    
+    else:
+        
+        str_num = str(num)
+        for i in str_num:
+            save_binary.append(i)
+            
+        bit_value = len(save_binary)-1
+        for i in save_binary:
+            if int(i) == 1:  
+                    z=int(i)
+                    z =  2**bit_value
+                    sum_bit_value +=z
+                    bit_value -= 1 
+                    if bit_value < 0:
+                        break
+            else:
+                bit_value -= 1
+                if bit_value < 0:
+                        break 
+        return sum_bit_value 
+          
 while True:
-    
-    menu_number = int(input("Welcome change to binary menu \n1)Change to binary\n2)exit\nselect your number :: "))
-    
+    print("\n" + "="*40)
+    menu_number = int(input("Welcome change to binary menu \n1)Change to binary\n2)Change to decimal\n3)exit\nselect your number :: "))
+    print("="*40)
     if menu_number == 1:
         num = int(input("Enter your number:: "))
         show_result = f"Answer is :: {change_to_binary(num)}"
         print(show_result)
-        
     elif menu_number == 2:
-        break
+        num = int(input("Enter your number:: "))
+        show_result = f"Answer is :: {change_to_decimal(num)}"
+        print(show_result)
+    elif menu_number == 3:
+       break
     else:
         continue
-    
-    
     
